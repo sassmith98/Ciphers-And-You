a810a6862d4d5670628204fd8ccebecfb50d0536
Steven Smith <sasmith10@student.ysu.edu>
2021-02-21 13:45:36 -0500

    Difficulty Addition

---
+++ src/difficultlyScreen.js
@@ -0,0 +1,24 @@
+import './App.js';
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Site by Steven Smith
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            START
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
